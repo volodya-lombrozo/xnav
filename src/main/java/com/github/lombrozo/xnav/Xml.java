dbb288da9876ed1e74efaf872aea4c2d689648df
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-01-20 16:00:07 +0300

    feat(#8): add new constructor and exctract common class StringNode

--- src/main/java/com/github/lombrozo/xnav/Xml.java
+++ src/main/java/com/github/lombrozo/xnav/Xml.java
@@ -23,14 +23,10 @@
  */
 package com.github.lombrozo.xnav;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Optional;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -41,7 +37,6 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 /**
  * XML abstraction over XML document.
@@ -69,7 +64,7 @@ final class Xml {
      * @param xml XML document as a string.
      */
     Xml(final String xml) {
-        this(Xml.fromString(xml));
+        this(new StringNode(xml).toNode());
     }
 
     /**
@@ -153,22 +148,5 @@ final class Xml {
             result = Optional.of(this.node).map(Node::getTextContent);
         }
         return result;
-    }
-
-    /**
-     * Create XML node from a string.
-     * @param xml XML as a string.
-     * @return XML node.
-     */
-    private static Node fromString(final String xml) {
-        try {
-            return Xml.DFACTORY.newDocumentBuilder()
-                .parse(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)));
-        } catch (final SAXException | IOException | ParserConfigurationException exception) {
-            throw new IllegalArgumentException(
-                String.format("Failed to parse XML: %s", xml),
-                exception
-            );
-        }
     }
 }
--- src/test/java/com/github/lombrozo/xnav/NavigatorTest.java
+++ src/test/java/com/github/lombrozo/xnav/NavigatorTest.java
@@ -46,6 +46,15 @@ final class NavigatorTest {
         );
     }
 
+    @Test
+    void createsNavigatorFromNode() {
+        MatcherAssert.assertThat(
+            "We expect the navigator to be created from node",
+            new Navigator(new StringNode("<a>text</a>").toNode()).toString(),
+            Matchers.equalTo("Navigator(node=<a>text</a>)")
+        );
+    }
+
     @ParameterizedTest
     @MethodSource({"elementPaths", "attributePaths"})
     void retrievesTextFromElements(final Navigator navigator, final String expected) {
