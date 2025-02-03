56ba8b9a622ec520f3fe452ed7a55497a4cb48f8
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-02-03 15:38:37 +0300

    feat(#39): find one more bug

--- src/test/java/com/github/lombrozo/xnav/XpathTest.java
+++ src/test/java/com/github/lombrozo/xnav/XpathTest.java
@@ -611,9 +611,10 @@ final class XpathTest {
                 "  <x a='true'>x1</x>",
                 "  <x a='true' b='false'>x2</x>",
                 "  <o base='Q.org.eolang.number'><o base='Q.org.eolang.bytes'>1-hex-content</o></o>",
-                "  <o base='Q.org.eolang.number'><o base='Q.org.eolang.bytes'>2-hex-content</o></o>",
-                "<o><o><o base='Q.org.eolang.string'><o base='Q.org.eolang.bytes'>first-</o><o>content</o></o></o></o>",
-                "<o base='Q.org.eolang.string'><o base='Q.org.eolang.bytes'>second-</o><o>content</o></o>",
+                "  <o base='org.eolang.bytes'><o base='bytes'>2-bytes-</o><o base='org.eolang.bytes'><o base='bytes'>content</o></o></o>",
+                "  <o><o base='Q.org.eolang.string'><o base='Q.org.eolang.bytes'>first-</o><o>content</o></o></o>",
+                "  <o><o><o base='Q.org.eolang.string'><o base='Q.org.eolang.bytes'>second-</o><o>content</o></o></o></o>",
+                "  <o base='Q.org.eolang.string'><o base='Q.org.eolang.bytes'>third-</o><o>content</o></o>",
                 "</root>"
             )
         );
@@ -623,7 +624,7 @@ final class XpathTest {
             {"(//x[@a and not(@b)])[1]", xml, "x1"},
             {"(//x[@a and not(@b)])[2]", xml, ""},
             {"(//x[@a and @b])[1]", xml, "x2"},
-            {"(//o[(@base='org.eolang.bytes' or @base='org.eolang.org.eolang.bytes') and(not(@skip)) and o[not(o) and string-length(normalize-space(text()))>0 and (@base='bytes' or @base='org.eolang.bytes')]])[1]", xml, "01"},
+            {"(//o[(@base='org.eolang.bytes' or @base='org.eolang.org.eolang.bytes') and(not(@skip)) and o[not(o) and string-length(normalize-space(text()))>0 and (@base='bytes' or @base='org.eolang.bytes')]])[1]", xml, "2-bytes-content"},
             {"(//o[@base='Q.org.eolang.number' and(not(@skip)) and o[1][@base='Q.org.eolang.bytes' and not(o) and string-length(normalize-space(text()))>0]])[1]", xml, "1-hex-content"},
             {"(//o[@base='Q.org.eolang.string' and(not(@skip)) and o[1][@base='Q.org.eolang.bytes' and not(o) and string-length(normalize-space(text()))>0]])[1]", xml, "first-content"},
         };
