489e568a0c93891d2fdb803827b8e8219a6be327
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-02-07 11:01:58 +0300

    chore: add tests for antlr xml

---
+++ src/test/java/com/github/lombrozo/xnav/AntlrXmlTest.java
@@ -0,0 +1,147 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Volodya Lombrozo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.lombrozo.xnav;
+
+import com.yegor256.Together;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AntlrXmlTest {
+
+    @Test
+    void convertsDocumentToString() {
+        MatcherAssert.assertThat(
+            "Document is not converted to string",
+            new AntlrXml("<doc></doc>").toString(),
+            Matchers.equalTo("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><doc/>")
+        );
+    }
+
+    @Test
+    void convertsNodeToString() {
+        MatcherAssert.assertThat(
+            "Node is not converted to string",
+            new AntlrXml("<doc><node>text</node></doc>").child("doc").child("node").toString(),
+            Matchers.equalTo("<node>text</node>")
+        );
+    }
+
+    @Test
+    void failsToCreateCorruptedDocument() {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> new AntlrXml("<doc..."),
+            "Corrupted document is not created, exception is expected"
+        );
+    }
+
+    @Test
+    void retrievesChildren() {
+        MatcherAssert.assertThat(
+            "Children are not retrieved",
+            new AntlrXml("<doc><node>first</node><node>second</node></doc>")
+                .child("doc")
+                .children()
+                .collect(Collectors.toList()),
+            Matchers.hasItems(
+                new AntlrXml("<node>first</node>").child("node"),
+                new AntlrXml("<node>second</node>").child("node")
+            )
+        );
+    }
+
+    @Test
+    void copiesNode() {
+        final Xml xml = new AntlrXml("<doc><node>text</node></doc>");
+        MatcherAssert.assertThat(
+            "Node is not copied",
+            xml.copy().toString(),
+            Matchers.equalTo(xml.toString())
+        );
+    }
+
+    @Test
+    void retrievesNode() {
+        MatcherAssert.assertThat(
+            "We expect the node to be retrieved",
+            new AntlrXml("<doc><node attr='value'>text</node></doc>")
+                .child("doc")
+                .child("node")
+                .node()
+                .isEqualNode(
+                    new StringNode("<node attr='value'>text</node>").toNode().getFirstChild()
+                ),
+            Matchers.is(true)
+        );
+    }
+
+    @Test
+    void retrievesObjects() {
+        MatcherAssert.assertThat(
+            "Objects are not retrieved",
+            new AntlrXml(
+                String.join(
+                    "\n",
+                    "<o>",
+                    "  <o color='red'>red</o>",
+                    "  <o color='blue'>blue</o>",
+                    "</o>"
+                )
+            ).child("o").children().filter(Filter.withName("o")).collect(Collectors.toList()),
+            Matchers.hasItems(
+                new AntlrXml("<o color='red'>red</o>").child("o"),
+                new AntlrXml("<o color='blue'>blue</o>").child("o")
+            )
+        );
+    }
+
+    @Test
+    void retrievesChildrenConcurrently() {
+        final Xml xml = new AntlrXml(
+            String.join(
+                "",
+                "<ob><o color='yellow'>yellow</o>",
+                "<o color='green'>green</o></ob>"
+            )
+        );
+        final int threads = 10;
+        final Together<List<Xml>> all = new Together<>(
+            threads,
+            indx -> xml.child("ob").children()
+                .flatMap(Xml::children)
+                .collect(Collectors.toList())
+        );
+        MatcherAssert.assertThat(
+            "Children are not retrieved concurrently",
+            all.asList().stream().flatMap(List::stream).collect(Collectors.toList()),
+            Matchers.hasSize(threads * 2)
+        );
+    }
+
+}
